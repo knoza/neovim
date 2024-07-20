6cd15d0a76ec93ed9e257dd3432c221e68a9bd1a
Knoza <knoza@pm.me>
2024-07-20 16:03:26 -0700

    add keymap for markdown preview

--- lua/knoza/lazy/markdown.lua
+++ lua/knoza/lazy/markdown.lua
@@ -1,6 +1,9 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  config = function ()
+    vim.keymap.set('n', '<leader>vm', ':MarkdownPreview<CR>')
+  end
 }
